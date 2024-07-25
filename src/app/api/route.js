6bf48d1ac5933cbc685b1a10c7eada66c3cf8237
nyxcrq <nyxcrq@gmail.com>
2024-07-25 15:02:23 +0700

    select data1

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,15 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+client.connect();
+
 export async function GET() {
     try {
-        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
+        const result = await client.query('SELECT * FROM tbl_users');
+        return new Response(JSON.stringify(result.rows), {
             status: 200,
             headers: { "Content-Type": "application/json" },
           });
