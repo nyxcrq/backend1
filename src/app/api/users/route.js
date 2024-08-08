27a39066a3daa528a7fbbfceaa9792ce997b9373
nyxcrq <nyxcrq@gmail.com>
2024-08-08 14:11:24 +0700

    080824login

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,6 @@
 import { Client } from 'pg';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
 dotenv.config();
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -11,32 +12,36 @@ export async function GET() {
         const result = await client.query('SELECT * FROM tbl_users');
         return new Response(JSON.stringify(result.rows), {
             status: 200,
-            headers: { "Content-Type": "application/json" },
+            headers: { 'Access-Control-Allow-Origin': '*',"Content-Type": "application/json" },
           });
 
         } catch (error) {
             return new Response(JSON.stringify({ error: error }), {
                 status: 500,
-                headers: { "Content-Type": "application/json" },
+                headers: { 'Access-Control-Allow-Origin': '*',"Content-Type": "application/json" },
               });
         }
     }
 
-export async function POST() {
-    try {
-        return new Response(JSON.stringify({ message: "POST DATA OK"}), {
-            status: 200,
-            headers: { "Content-Type": "application/json" },
-          });
-          
-        } catch (error) {
-            return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-                status: 500,
-                headers: { "Content-Type": "application/json" },
-              });
-        }
-    }
-
+export async function POST(request) {
+      try {
+      const { firstname, lastname, username, password } = await request.json();
+      // Hash password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+      const res = await client.query('INSERT INTO tbl_users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4) RETURNING *', [firstname, lastname, username, hashedPassword]);
+      return new Response(JSON.stringify(res.rows[0]), {
+      status: 201,
+      headers: { 'Content-Type': 'application/json' },
+      });
+      } catch (error) {
+      console.error(error);
+      return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+      });
+      }
+      }
 export async function PUT() {
     try {
         return new Response(JSON.stringify({ message: "PUT DATA OK"}), {
